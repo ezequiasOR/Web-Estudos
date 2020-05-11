e9bdfaef579b2f59249868042cd1a51954fdde67
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-05-11 10:20:53 -0300

    webpack: update 1

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,21 @@ module.exports = {
   output: {
     filename: 'principal.js',
     path: __dirname + '/public'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.s?[ac]ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        // 'style-loader',   // Adiciona CSS a DOM injetando a tag <style>
+        'css-loader',  // interpreta @import, url()...
+        'sass-loader',
+      ]
+    }]
   }
 }
