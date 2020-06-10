a6bad8bcc4169f73673d31cb3605379b93c88601
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-06-10 09:49:53 -0300

    react: update 1

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React</h1> 
+import Multi, { BoaNoite } from './componentes/Multiplos'
 
-ReactDOM.render(elemento, document.getElementById('root'))
+ReactDOM.render(
+  <div>
+    <Multi.BoaTarde nome='Ana' />
+    <BoaNoite nome='Bia' />
+  </div>  
+, document.getElementById('root'))
