16feb86ca7f4bd542a907178d3811fd065b75cb2
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-08-29 11:01:24 -0300

    electron/vue: update 2

--- most-used-words/src/components/Home.vue
+++ most-used-words/src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as Legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+        />
+    </v-form>
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name"
         :name="word.name" :amount="word.amount" />
@@ -8,32 +20,25 @@
 </template>
 
 <script>
+import { ipcRenderer } from 'electron'
 import Pill from './Pill'
 
 export default {
   components: { Pill },
   data: function() {
     return {
-      groupedWords: [
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-      ]
+      files: [],
+      groupedWords: []
+    }
+  },
+  methods: {
+    processSubtitles() {
+      const paths = this.files.map(f => f.path)
+      
+      ipcRenderer.send('process-subtitles', paths)
+      ipcRenderer.on('process-subtitles', (event, resp) => {
+        this.groupedWords = resp
+      })
     }
   }
 }
