9bd493df9e4563025f792ea927b079cdf0c0b083
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-12-21 13:38:07 -0300

    banco relacional

---
+++ banco-relacional/consultarEstados.sql
@@ -0,0 +1,8 @@
+SELECT * FROM estados;
+
+SELECT sigla, nome AS 'Nome do estado' FROM estados
+WHERE regiao = 'Sul';
+
+SELECT nome, regiao, populacao FROM estados
+WHERE populacao >= 10
+ORDER BY populacao desc;
