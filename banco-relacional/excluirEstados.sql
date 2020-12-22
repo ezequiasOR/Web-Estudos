5e09698e14d80e4ac824cae6fb9bdfbca7525e83
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-12-22 11:39:02 -0300

    banco relacional: update 1

---
+++ banco-relacional/excluirEstados.sql
@@ -0,0 +1,7 @@
+DELETE FROM estados
+WHERE sigla = 'NE';
+
+DELETE FROM estados
+WHERE id >= 1000;
+
+SELECT * FROM estados;
---
+++ banco-relacional/inserirCidades.sql
@@ -0,0 +1,19 @@
+SELECT * from estados;
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Campinas', 795, 30);
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Niterói', 133.9, 24);
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Caruaru', 920.6, 
+  (SELECT id FROM estados WHERE sigla = 'PE')
+);
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Juazeiro do Norte', 248.2,
+  (SELECT id FROM estados WHERE sigla = 'CE')
+);
+
+SELECT * FROM cidades;
