3f5b1a57808a66a1736e7bb6b268fa79db486ea1
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-04 11:41:26 -0300

    express: update 1

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,8 +1,42 @@
 const express = require('express')
 const app = express()
 
-app.get('/opa', (req, res) => {
-  res.send('Estou bem!')
+const saudacao = require('./saudacaoMid')
+
+app.use(saudacao('João'))
+
+app.use('/opa', (req, res, next) => {
+  console.log('Antes...')
+  next()
+})
+
+app.get('/opa', (req, res, next) => {
+  console.log('Durante...')
+  res.json({
+    data: [
+      {id: 7, name: 'Ana', position: 1},
+      {id: 34, name: 'Bia', position: 2},
+      {id: 73, name: 'Carlos', position: 3}
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200
+  })
+
+  next()
+
+  // res.json({
+  //   name: 'iPad 32 Gb',
+  //   price: 1899.00,
+  //   discount: 0.12
+  // })
+
+  // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+})
+
+app.use('/opa', (req, res) => {
+  console.log('Depois...')
 })
 
 app.listen(3000, () => {
