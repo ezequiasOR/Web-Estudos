933f120b758e3cee1c1a4a95a376bf2b8a46a808
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-05 12:16:30 -0300

    express: update 2

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+require('./api/produto')(app, 'com param!')
 
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(saudacao('João'))
 
 app.use('/opa', (req, res, next) => {
   console.log('Antes...')
   next()
+})
+
+app.get('/clientes/relatorio', (req, res) => {
+  res.send(`Cliente relarório: completo = ${req.query.completo}, ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+  // let corpo = ''
+  // req.on('data', function(parte) {
+  //   corpo += parte
+  // })
+
+  // req.on('end', function() {
+  //   res.send(corpo)
+  // })
+  res.send(req.body)
+})
+
+app.get('/clientes/:id', (req, res) => {
+  res.send(`Cliente ${req.params.id} selecionado!`)
 })
 
 app.get('/opa', (req, res, next) => {
